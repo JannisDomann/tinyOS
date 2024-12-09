87586dc8e37e25343243e3e69089fe58be9e8829
Jannis Domann <jannisdomann@gmail.com>
2024-12-09 21:55:05 +0100

    main with 6 tasks (incl. idle)

--- main.cpp
+++ main.cpp
@@ -1,60 +1,67 @@
 #include "tOS_Kernel.h"
-// #include "tOS_Queue.h"
+#include "tOS_Queue.h"
 
 tOS_Kernel kernel;
-// tOS_Queue<tOS_String> q;
+tOS_Queue<tOS_String> q;
 
-// void task1() {
-// 	for (;;) {
-// 		RNLIB::setLed(0x01);
-// 		printf("led1 on\n");
-// 		kernel.delay_ms(2356);
-// 		RNLIB::clearLed(0x01);
-// 		printf("led1 off\n");
-// 		kernel.delay_ms(1856);
-// 	}
-// }
+void task1() {
+	for (;;) {
+		RNLIB::setLed(0x00);
+		q.put("led1 on\n");
+		kernel.delay_ms(2356);
+		RNLIB::clearLed(0x00);
+		q.put("led1 off\n");
+		kernel.delay_ms(1856);
+	}
+}
 
-// void task2() {
-// 	for (;;) {
-// 		RNLIB::setLed(0x02);
-// 		printf("led2 on\n");
-// 		kernel.delay_ms(345);
-// 		RNLIB::clearLed(0x02);
-// 		printf("led2 off\n");
-// 		kernel.delay_ms(635);
-// 	}
-// }
+void task2() {
+	for (;;) {
+		RNLIB::setLed(0x02);
+		q.put("led2 on\n");
+		kernel.delay_ms(345);
+		RNLIB::clearLed(0x02);
+		q.put("led2 off\n");
+		kernel.delay_ms(635);
+	}
+}
 
 void task3() {
 	for (;;) {
-		printf("led3 on\n");
+		RNLIB::setLed(0x03);
+		q.put("led3 on\n");
+		kernel.delay_ms(1000);
+		RNLIB::clearLed(0x03);
+		q.put("led3 off\n");
 		kernel.delay_ms(1000);
 	}
 }
 
 void task4() {
 	for (;;) {
-		printf("led4 on\n");
-		kernel.delay_ms(963);
+		RNLIB::setLed(0x01);
+		q.put("led4 on\n");
+		kernel.delay_ms(120);
+		RNLIB::clearLed(0x01);
+		q.put("led4 off\n");
+		kernel.delay_ms(200);
 	}
 }
 
-// void printTask() {
-// 	for (;;) {
-// 		if (q.size() > 0) {
-// 			printf(q.front());
-// 			q.pop();
-// 		}
-// 	}
-// }
+void printTask() {
+	for (;;) {
+		if (q.pending())
+			printf(q.get());
+		kernel.delay_ms(1);
+	}
+}
 
 int main() {
-	// kernel.createTask(tOS_Task("task1", task1, tOS_Prio_UPPERNORMAL));
-	// kernel.createTask(tOS_Task("task2", task2));
+	kernel.createTask(tOS_Task("print", printTask));
+	kernel.createTask(tOS_Task("task2", task2));
+	kernel.createTask(tOS_Task("task4", task4, tOS_Prio_LOWERNORMAL));
+	kernel.createTask(tOS_Task("task1", task1, tOS_Prio_REALTIME));
 	kernel.createTask(tOS_Task("task3", task3, tOS_Prio_UPPERNORMAL));
-	kernel.createTask(tOS_Task("task4", task4, tOS_Prio_UPPERNORMAL));
-	// kernel.createTask(tOS_Task("print", printTask));
 
 	kernel.start();
  
